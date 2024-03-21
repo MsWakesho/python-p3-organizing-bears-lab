d8a5323d905f38d2dcfbc868c52fae49dc315687
MsWakesho <wakeshosonja@gmail.com>
2024-03-22 00:25:41 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 3, "F", "chocolate", "moody", 1),
+("Rowdy", 2, "M", "ginger", "happy", 1),
+("Tabitha", 20, "F", "chocolate", "at peace", 0),
+("Sergeant Brown", 11, "M", "white", "strict", 1),
+("Melissa", 5, "M", "red", "perky", 1),
+("Grinch", 8, "M", "green", "sneaky", 1),
+("Wendy", 10, "M", "black", "cool", 1),
+(NULL, 12, "F", "orange", "angry", 1);
